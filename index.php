1be7fb06a7571fa309ddbe1f83e3cc28718e0c5a
Nguyễn Minh Đức <nguyenminhduc2001pt@gmail.com>
2021-08-19 21:30:08 +0700

    import he thong chuyen huong php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+$page = !empty($_GET['page'])?$_GET['page']:"login";
+
+$path = "pages/{$page}.html";
+#4
+if(file_exists($path))
+{
+    require $path;
+}else{
+    require "pages/404.html";
+}
+?>
+
