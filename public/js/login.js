72618d2a08af086a624b3d846e872035b78408c5
Nguyễn Minh Đức <nguyenminhduc2001pt@gmail.com>
2021-08-21 17:48:36 +0700

    update login.js gio han ky tu

--- public/js/login.js
+++ public/js/login.js
@@ -12,7 +12,13 @@ $(document).ready(function () {
                 if (email == '' || password == '') {
                     error = 1;
                     alert = "Không để trường trống";
-                } else {
+                }else if (email.length > 50 ) {
+                    error = 1;
+                    alert = "Email Chỉ Giới Hạn 50 Ký Tự";
+                }else if (password.length > 30){
+                    error = 1;
+                    alert = "Password Chỉ Giới Hạn 30 Ký Tự";
+                }else {
                     for (let i = 0; i < data.length; i++) {
                         if (email != data[i].email && password != data[i].password) {
                             error = 1;
