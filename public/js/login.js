595a4306f61fb69ffa2e12ac1e94dbe3bd9c37f2
Nguyễn Minh Đức <nguyenminhduc2001pt@gmail.com>
2021-08-21 18:12:43 +0700

    login kiem tra dinh dang email dang nhap

--- public/js/login.js
+++ public/js/login.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $("#btn-login").click(function () {
         var email = $("#email").val();
         var password = $("#password").val();
+        var filter_email = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; 
         var error = 0;
         var alert;
         $.ajax({
@@ -18,6 +19,10 @@ $(document).ready(function () {
                 }else if (password.length > 30){
                     error = 1;
                     alert = "Password Chỉ Giới Hạn 30 Ký Tự";
+                }else if (!filter_email.test(email)){
+                    error = 1;
+                    alert = "Email Không Đúng Định Dạng";
+
                 }else {
                     for (let i = 0; i < data.length; i++) {
                         if (email != data[i].email && password != data[i].password) {
@@ -34,6 +39,7 @@ $(document).ready(function () {
                 } else {
                     $(".error").removeClass("d-none");
                     $(".error").text(alert);
+                    $("#password").val("");
                 }
             },
             error: function () {
