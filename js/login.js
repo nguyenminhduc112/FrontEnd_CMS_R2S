3f34971a9707ac650f695cdd3d6699d3d8f995ba
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-08-21 12:31:00 +0700

    Rename edit.html to edit-profile.html

--- js/login.js
+++ js/login.js
@@ -2,6 +2,6 @@ const loginForm = document.getElementById('login-form')
 
 loginForm.addEventListener('submit', (event) => {
 	event.preventDefault()
-	window.location.replace('edit.html')
+	window.location.replace('edit-profile.html')
 	return false
 })
